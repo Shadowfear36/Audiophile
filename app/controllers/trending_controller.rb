4e0f101e553e1386252bf304903ca040708532f2
DylanRhinehart <dylanrhinehart@gmail.com>
2023-03-24 12:19:00 -0700

    fixed trending to look at not only likes count but comment count as well

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,23 +1,26 @@
 class TrendingController < ApplicationController
 
     def results
-      @albums =  Album.select("albums.*, COUNT(poly_likes.id) AS likes")
-     .joins("LEFT JOIN poly_likes ON poly_likes.likeable_id = albums.id AND poly_likes.likeable_type = 'Album'")
-     .group("albums.id")
-     .order("likes DESC")
-     .limit(10)
+    @albums = Album.select("albums.*, COUNT(poly_likes.id) AS likes, COUNT(poly_comments.id) AS comments")
+    .joins("LEFT JOIN poly_likes ON poly_likes.likeable_id = albums.id AND poly_likes.likeable_type = 'Album'")
+    .joins("LEFT JOIN poly_comments ON poly_comments.commentable_id = albums.id AND poly_comments.commentable_type = 'Album'")
+    .group("albums.id")
+    .order("likes DESC, comments DESC")
+    .limit(10)
 
-     @songs =  Song.select("songs.*, COUNT(poly_likes.id) AS likes")
-     .joins("LEFT JOIN poly_likes ON poly_likes.likeable_id = songs.id AND poly_likes.likeable_type = 'Song'")
-     .group("songs.id")
-     .order("likes DESC")
-     .limit(10)
+    @songs = Song.select("songs.*, COUNT(poly_likes.id) AS likes, COUNT(poly_comments.id) AS comments")
+    .joins("LEFT JOIN poly_likes ON poly_likes.likeable_id = songs.id AND poly_likes.likeable_type = 'Song'")
+    .joins("LEFT JOIN poly_comments ON poly_comments.commentable_id = songs.id AND poly_comments.commentable_type = 'Song'")
+    .group("songs.id")
+    .order("likes DESC, comments DESC")
+    .limit(10)
 
-     @playlists =  Playlist.select("playlists.*, COUNT(poly_likes.id) AS likes")
-     .joins("LEFT JOIN poly_likes ON poly_likes.likeable_id = playlists.id AND poly_likes.likeable_type = 'Playlist'")
-     .group("playlists.id")
-     .order("likes DESC")
-     .limit(10)
+    @playlists = Playlist.select("playlists.*, COUNT(poly_likes.id) AS likes, COUNT(poly_comments.id) AS comments")
+    .joins("LEFT JOIN poly_likes ON poly_likes.likeable_id = playlists.id AND poly_likes.likeable_type = 'Playlist'")
+    .joins("LEFT JOIN poly_comments ON poly_comments.commentable_id = playlists.id AND poly_comments.commentable_type = 'Playlist'")
+    .group("playlists.id")
+    .order("likes DESC, comments DESC")
+    .limit(10)
 
      render json: {albums: @albums, songs: @songs, playlists: @playlists}, status: :ok
     end
